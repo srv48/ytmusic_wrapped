6291729d78b2d83ef9834099e8a0456b78346e8d
cinfulsinamon <48000371+cinfulsinamon@users.noreply.github.com>
2019-12-10 11:45:16 -0700

    python3 compatible version
    change how json is opened and how files are written to account for python3's Unicode formatting. more status messages in console as well

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import re
 expect = datetime.datetime.now().year
 verbose = False
 duration = False
+log = open('log.dat', 'w', encoding="utf8")
 
 def flags():
 	opts, args = getopt.getopt(sys.argv[2:], "d:v", ["duration="])
@@ -22,13 +23,13 @@ def flags():
 			ytAPIkey = token
 
 def i18n_string(string):
-	if (string[:7].encode("utf-8") == "Watched"):
+	if (string[:7] == "Watched"):
 		return True
 	else:
 		return False
 
 def i18n_title(title):
-	if (title[:7].encode("utf-8") == "Watched"):
+	if (title[:7] == "Watched"):
 		return title[8:]
         
 def il8n_url(url):
@@ -36,9 +37,9 @@ def il8n_url(url):
     return str
 
 def should_not_ignore(title, year, header, expect):
-    if (header.encode("utf-8") == "YouTube Music"):
+    if (header == "YouTube Music"):
         if (i18n_string(title)):
-            if (year[:4].encode("utf-8") == str(expect)):
+            if (year[:4] == str(expect)):
                 return True
             else:
                 False
@@ -50,13 +51,13 @@ def should_not_ignore(title, year, header, expect):
 def open_file():
 	if (sys.argv[1].endswith('.json')):
 		try:
-			file = open(sys.argv[1], "r")
+			file = open(sys.argv[1], "r", encoding="utf8")
 			return file
 		except:
-			print "Could not open your history file"
+			print("Could not open your history file")
 			sys.exit()
 	else:
-		print "Your history file should be a json file"
+		print("Your history file should be a json file")
 		sys.exit()
 
 def parse_json(file, cursor):
@@ -70,18 +71,18 @@ def parse_json(file, cursor):
 
 def print_db(cursor):
 	#Print results from DB
-    print ("####################Full List#####################")
+    print ("####################Full List#####################", file = log)
     cursor.execute("""SELECT id, artist, title, url, year FROM songs""")
     rows = cursor.fetchall()
     for row in rows:
         datetime.datetime.now()
-        print('{0} : {1} - {2} - {4} - {3}'.format(row[0], row[1].encode("utf-8"), row[2].encode("utf-8"), row[3].encode("utf-8"), row[4]))
-    print ("####################Non-Duplicate List#####################")
+        print('{0} : {1} - {2} - {4} - {3}'.format(row[0], row[1], row[2], row[3], row[4]), file = log)
+    print ("####################Non-Duplicate List#####################", file = log)
     cursor.execute("""SELECT id, artist, title, url, occurence FROM report""")
     rows = cursor.fetchall()
     for row in rows:
         datetime.datetime.now()
-        print('{0} : {1} - {2} - {3} - {4}'.format(row[0], row[1].encode("utf-8"), row[2].encode("utf-8"), row[3].encode("utf-8"), row[4]))
+        print('{0} : {1} - {2} - {3} - {4}'.format(row[0], row[1], row[2], row[3], row[4]), file = log)
 
 def prepare_tops(cursor):
 	#Artist top
@@ -110,7 +111,7 @@ def delete_duplicate(cursor):
     cursor.execute("""SELECT id, artist, title, url FROM report""")
     rows = cursor.fetchall()
     for row in rows:
-        if (row[2].encode("utf-8") == "parseme"):
+        if (row[2] == "parseme"):
            cursor.execute("""SELECT artist, title FROM songs WHERE url = ? AND title != ?""",(row[3],"parseme"))
            match = cursor.fetchone()
            if match:
@@ -119,22 +120,22 @@ def delete_duplicate(cursor):
         cursor.execute("""DELETE FROM report WHERE title = 'parseme'""")
         
 def print_full_tops(cursor):
-	print ("####################Top Artists#####################")
+	print ("####################Top Artists#####################", file = log)
 	cursor.execute("""SELECT artist, occurence FROM artist_count ORDER by occurence DESC""")
 	rows = cursor.fetchall()
 	for row in rows:
 		datetime.datetime.now()
-		print('{0} - {1}'.format(row[0].encode("utf-8"), row[1]))
+		print('{0} - {1}'.format(row[0], row[1]), file = log)
 
-	print ("####################Top Songs#####################")
+	print ("####################Top Songs#####################", file = log)
 	cursor.execute("""SELECT title, occurence FROM songs_count ORDER by occurence DESC""")
 	rows = cursor.fetchall()
 	for row in rows:
 		datetime.datetime.now()
-		print('{0} - {1}'.format(row[0].encode("utf-8"), row[1]))
+		print('{0} - {1}'.format(row[0], row[1]), file = log)
         
 def parse_duration(duration):
-    timestr = duration.encode("utf-8")
+    timestr = duration
     time = re.findall(r'\d+', timestr) 
     length = len(time)
     if length > 4:
@@ -151,7 +152,7 @@ def parse_duration(duration):
         return 0
         
 def call_api(idlist, cursor):
-    print "api called"
+    print("api called", file = log)
     parameters = {"part": "contentDetails,snippet", "id": ','.join(idlist), "key": ytAPIkey}
     response = requests.get("https://www.googleapis.com/youtube/v3/videos", params=parameters)
     if (response.status_code == 200):
@@ -167,20 +168,25 @@ def get_duration(cursor):
     #Count duration
     cursor.execute("""SELECT id, artist, title, url FROM report""")
     rows = cursor.fetchall()
+    print("\tNumber of videos: " + str(len(rows)))
     idlist = []
+    calls = 0
     for row in rows:
-        idlist.append(row[3].encode("utf-8"))
+        idlist.append(row[3])
         if len(idlist) == 50:
-            print ','.join(idlist)
+            print("\tGetting info on videos " + str(1+50*calls) + " - " + str(50+50*calls))
+            print(','.join(idlist), file = log)
             call_api(idlist, cursor)
+            calls = calls + 1
             idlist = []
-    print ','.join(idlist)
+    print("\tGetting info on videos " + str(1+50*calls) + " - " + str(len(rows)))
+    print(','.join(idlist), file = log)
     call_api(idlist, cursor)
     cursor.execute("""UPDATE report SET duration = ?, artist = ?, title = ? WHERE title = ?""", (0, "Unknown Artist", "Unavailable Video", "parseme"))
     
     #Calcul total duration
     if verbose:
-        print ("####################Full List WITHOUT DOUBLON AND DURATION#####################")
+        print ("####################Full List WITHOUT DOUBLON AND DURATION#####################", file = log)
     song_count = 0
     total_duration = 0
     error_rate = 0
@@ -190,60 +196,60 @@ def get_duration(cursor):
         datetime.datetime.now()
         song_count = row[0]
         if verbose:
-            print('{0} : {1} - {2}- {3} - occurence : {4} - {5}'.format(row[0], row[1].encode("utf-8"), row[2].encode("utf-8"), row[3], row[4], row[5].encode("utf-8")))
+            print('{0} : {1} - {2}- {3} - occurence : {4} - {5}'.format(row[0], row[1], row[2], row[3], row[4], row[5]), file = log)
         total_duration += row[3] * row[4]
         if row[3] == 0:
             error_rate = error_rate + 1
     return (total_duration, error_rate, song_count)
 
 def gen_html_report(cursor, data, expect):
-	sys.stdout = open('report.html', 'w')
-	print ("""<!DOCTYPE html><html><head><title>Wrapped</title><style type="text/css">body{background-color: #000000;}.center-div{position: absolute; margin: auto; top: 0; right: 0; bottom: 0; left: 0; width: 50%; height: 90%; background-color: #000000; border-radius: 3px; padding: 10px;}.ytm_logo{width: 15%;position: relative;top: 30px;left: 40px;}.title_logo{width: 30%;position: relative;top: 30px;left: 60px;}.right_title{position: absolute;font-family: "Product Sans";top: 55px;right: 10%;font-size: 2em;color: #ffffff;}.container{position: relative;top: 13%;left: 53px;}.minutes_title{font-family: "Product Sans";font-size: 2em;color: #ffffff;}.minutes{font-family: "Product Sans";font-size: 6em;color: #ffffff;}.row{display: flex;}.column{flex: 50%;}.list{font-family: "Roboto";font-size: 1.5em;line-height: 30px;color: #ffffff;}</style></head><body><div class="center-div"><img src="ytm_logo.png" class="ytm_logo"><img src="title.png" class="title_logo"/><span class="right_title">""")
-	print (expect)
-	print (""" Wrapped</span><div class="container"><div class="minutes_title">Minutes Listened</div><div class="minutes">""")
+	htmlreport = open('report.html', 'w', encoding = ("utf8"))
+	print ("""<!DOCTYPE html><html><head><title>Wrapped</title><style type="text/css">body{background-color: #000000;}.center-div{position: absolute; margin: auto; top: 0; right: 0; bottom: 0; left: 0; width: 50%; height: 90%; background-color: #000000; border-radius: 3px; padding: 10px;}.ytm_logo{width: 15%;position: relative;top: 30px;left: 40px;}.title_logo{width: 30%;position: relative;top: 30px;left: 60px;}.right_title{position: absolute;font-family: "Product Sans";top: 55px;right: 10%;font-size: 2em;color: #ffffff;}.container{position: relative;top: 13%;left: 53px;}.minutes_title{font-family: "Product Sans";font-size: 2em;color: #ffffff;}.minutes{font-family: "Product Sans";font-size: 6em;color: #ffffff;}.row{display: flex;}.column{flex: 50%;}.list{font-family: "Roboto";font-size: 1.5em;line-height: 30px;color: #ffffff;}</style></head><body><div class="center-div"><img src="ytm_logo.png" class="ytm_logo"><img src="title.png" class="title_logo"/><span class="right_title">""", file = htmlreport)
+	print (str(expect), file = htmlreport)
+	print (""" Wrapped</span><div class="container"><div class="minutes_title">Minutes Listened</div><div class="minutes">""", file = htmlreport)
 	if duration:
-		print (data[0]/60)
+		print (str(data[0]/60), file = htmlreport)
 	else:
-		print("N/A")
-	print ("""</div><br><br><div class="row"><div class="column"><div class="minutes_title">Top Artists</div><div class="list">""")
+		print("N/A", file = htmlreport)
+	print ("""</div><br><br><div class="row"><div class="column"><div class="minutes_title">Top Artists</div><div class="list">""", file = htmlreport)
 	cursor.execute("""SELECT artist, occurence FROM artist_count ORDER by occurence DESC LIMIT 10""")
 	rows = cursor.fetchall()
 	for row in rows:
-		print ("<br>")
-		print('{0} - {1} songs'.format(row[0].encode("utf-8"), row[1]))
-	print ("""</div></div><div class="column"><div class="minutes_title">Top Songs</div><div class="list">""")
+		print ("<br>", file = htmlreport)
+		print('{0} - {1} songs'.format(row[0], row[1]), file = htmlreport)
+	print ("""</div></div><div class="column"><div class="minutes_title">Top Songs</div><div class="list">""", file = htmlreport)
 	cursor.execute("""SELECT title, occurence FROM songs_count ORDER by occurence DESC LIMIT 10""")
 	rows = cursor.fetchall()
 	for row in rows:
-		print ("<br>")
-		print ('{0} - {1} plays'.format(row[0].encode("utf-8"), row[1]))
-	print ("""</div></div></div></div></div></body></html>""")
-	sys.stdout.close()
+		print ("<br>", file = htmlreport)
+		print ('{0} - {1} plays'.format(row[0], row[1]), file = htmlreport)
+	print ("""</div></div></div></div></div></body></html>""", file = htmlreport)
+	htmlreport.close()
 
 def gen_report(cursor, data, expect):
 	#Top 10 Report
-	sys.stdout = open('report.dat', 'w')
-	print ("#################### Top Artists #####################")
+	report = open('report.dat', 'w', encoding=("utf8"))
+	print ("#################### Top Artists #####################", file = report)
 	cursor.execute("""SELECT artist, occurence FROM artist_count ORDER by occurence DESC""")
 	rows = cursor.fetchall()
 	for row in rows:
 		datetime.datetime.now()
-		print('{0} - {1}'.format(row[0].encode("utf-8"), row[1]))
+		print('{0} - {1}'.format(row[0], row[1]), file = report)
 
-	print ("#################### Top Songs #####################")
+	print ("#################### Top Songs #####################", file = report)
 	cursor.execute("""SELECT title, occurence FROM songs_count ORDER by occurence DESC""")
 	rows = cursor.fetchall()
 	for row in rows:
 		datetime.datetime.now()
-		print('{0} - {1}'.format(row[0].encode("utf-8"), row[1]))
+		print('{0} - {1}'.format(row[0], row[1]), file = report)
 
 	if duration:
-		print ("\n#################### Duration #####################")
-		print ('Total duration : {0}', data[0])
-		print ('Total song count : ', data[2])
-		print ('Error count : ', data[1])
-		print ('Error rate : {0}%'.format((float(data[1])/data[2])*100))
-	sys.stdout.close()
+		print ("\n#################### Duration #####################", file = report)
+		print ('Total duration : {0}'.format(data[0]), file = report)
+		print ('Total song count : '.format(data[2]), file = report)
+		print ('Error count : '.format(data[1]), file = report)
+		print ('Error rate : {0}%'.format((float(data[1])/data[2])*100), file = report)
+	report.close()
 	gen_html_report(cursor, data, expect)
 
 def main():
@@ -258,23 +264,24 @@ def main():
 
     print ("Welcome to YouTube Music Year Wrapper.")
     print ("We are now processing your file.")
-    print ("No more informations will be displayed during this process. You can check log.dat at any time to check progression.")
-
-    if verbose:
-        sys.stdout = open('log.dat', 'w')
+    
     parse_json(file, cursor)
+    print("Removing duplicates")
     delete_duplicate(cursor)
     
     if verbose:
         print_db(cursor)
-	if duration:
-		data = get_duration(cursor)
+    if duration:
+        print("Getting durations. This may take a while.")
+        data = get_duration(cursor)
+    print("Getting top 10's")
     prepare_tops(cursor)
     if verbose:
-		print_full_tops(cursor)
-    if verbose:
-        sys.stdout.close()
+        print_full_tops(cursor)
+    log.close()
+    print("Generating final report")
     gen_report(cursor, data, expect)
+    print("All done!")
 
 if __name__ == "__main__":
 	main()
